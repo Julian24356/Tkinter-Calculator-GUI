39bec1ef2e99362631b7ee826b07eed94117095c
Julian24356 <iulian.prodan158@gmail.com>
2024-10-20 19:41:59 +0300

    Add expression field

--- Calculator-app.py
+++ Calculator-app.py
@@ -2,11 +2,12 @@ import tkinter as tk
 
 # config
 root = tk.Tk()
-root.geometry("400x400")
+root.geometry("300x300")
 root.title("Calculator")
 root.configure(background="sky blue")
 
 # print(dir(root))
-result = tk.Label(root, text="Test", font=("Arial",18), background="sky blue", justify="left") # add border=
-result.pack(anchor="w") # add padding pady=10, padx=10,
+equation = tk.StringVar()
+expression_field = tk.Entry(root, textvariable=equation,background="sky blue", font=("Arial",18,'bold'))
+expression_field.grid(columnspan=4, ipadx=70)
 root.mainloop()
